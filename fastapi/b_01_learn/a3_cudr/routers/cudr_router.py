f20072f5a5c195487c6cea6ac59eddbc25875449
CodingSoldier <tfz9011@163.com>
2026-03-31 22:35:30 +0800

    cudr

--- fastapi/b_01_learn/a3_cudr/routers/cudr_router.py
+++ fastapi/b_01_learn/a3_cudr/routers/cudr_router.py
@@ -1,15 +1,24 @@
-from fastapi import APIRouter, Depends
+from pydoc import describe
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.params import Query, Body, Path
+from sqlalchemy import select, update, delete
 from sqlalchemy.orm import Session
 from a2_sqlalchemy.db_config import get_session
 from models import Employee, SexValue
-from a3_cudr.schemas.emp import EmpAddReq, EmpAddResp
+from a3_cudr.schemas.emp import EmpAddReq, EmpResp, EmpDeptResp, EmpUpReq
 
 cudr = APIRouter(prefix='/cudr', tags=['增删改查接口'])
 
 
-@cudr.post('/employee', response_model=EmpAddResp)
+@cudr.post('/employee', response_model=EmpResp)
 def add_employee(req: EmpAddReq, session: Session = Depends(get_session)):
     """新增员工"""
+    db_emp = session.execute(select(Employee).where(Employee.name == req.name)).first()
+    if db_emp:
+        raise HTTPException(status_code=405, detail="员工姓名已经存在，请修改姓名再提交")
+
     # 使用 model_dump 将请求数据转换为字典，并添加默认值
     emp_data = req.model_dump()
     
@@ -26,4 +35,55 @@ def add_employee(req: EmpAddReq, session: Session = Depends(get_session)):
     session.refresh(emp)
     
     # 返回响应，使用 model_validate 方法
-    return EmpAddResp.model_validate(emp)
+    return EmpResp.model_validate(emp)
+
+
+@cudr.get(path="/emp/detail/{id}", description="查询单个员工", response_model=EmpDeptResp)
+def emp_detail(id: int, session: Session=Depends(get_session)):
+    db_emp = session.get(Employee, id)
+    if db_emp:
+        return db_emp
+    else:
+        raise HTTPException(status_code=401, detail="没有数据")
+
+
+@cudr.get(path="/page", response_model=List[EmpResp])
+def get_page(
+        page_index: int = Query(default=1, description="页码"),
+        page_size: int = Query(default=10, description="页数"),
+        session: Session=Depends(get_session)):
+    offset = (page_index - 1) * page_size
+    return session.scalars(select(Employee).offset(offset).limit(page_size)).all()
+
+
+@cudr.put("/emp/update")
+def emp_update(
+        emp_up: EmpUpReq,
+        session: Session = Depends(get_session)):
+    # 将请求数据转换为字典
+    update_data = emp_up.model_dump(
+        exclude_unset=True,
+        exclude_none=True
+    )
+    
+    # 如果包含 gender 字段，需要将其转换为 SexValue 枚举
+    if 'gender' in update_data:
+        update_data['gender'] = SexValue(update_data['gender'])
+    
+    stmt = update(Employee).where(Employee.id == emp_up.id).values(**update_data)
+    session.execute(stmt)
+    session.commit()
+    return "success"
+
+@cudr.delete(path="/emp/{id}", description="删除员工")
+def delete_emp(
+        id: int = Path(description="员工id"),
+        session: Session = Depends(get_session)):
+    session.execute(delete(Employee).where(Employee.id == id))
+    session.commit()
+    return "success"
+
+
+
+
+
--- fastapi/b_01_learn/a3_cudr/schemas/emp.py
+++ fastapi/b_01_learn/a3_cudr/schemas/emp.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field, ConfigDict
 from datetime import date
 
@@ -10,11 +12,11 @@ class EmpAddReq(BaseModel):
     sal: float = Field(description="员工基本工资", alias="salary")
     bonus: int = Field(default=0, description="员工津贴和奖金")
     is_leave: bool = Field(default=False, description="员工是否离职")
-    gender: str = Field(default="男", description="员工性别")
+    gender: str = Field(default="男", description="员工性别", pattern="^(男 | 女)$")
     entry_date: date = Field(description="入职日期")
 
 
-class EmpAddResp(BaseModel):
+class EmpResp(BaseModel):
     """新增员工响应模型"""
     model_config = ConfigDict(from_attributes=True)
     
@@ -25,3 +27,30 @@ class EmpAddResp(BaseModel):
     is_leave: bool = Field(description="员工是否离职")
     gender: str = Field(description="员工性别")
     entry_date: date = Field(description="入职日期")
+
+class DeptResp(BaseModel):
+    """部门响应体"""
+    id: int = Field(description="部门的id")
+    name: str = Field(description="部门名称")
+    city: str = Field(description="所在城市")
+
+
+class EmpDeptResp(EmpResp):
+    """员工、部门信息"""
+
+    model_config = ConfigDict(from_attributes=True)
+
+    dept_id: Optional[int] = Field(description="员工的部门id")
+
+    dept: Optional[DeptResp] = Field(description="部门信息")
+
+class EmpUpReq(BaseModel):
+    """员工更新"""
+    id: int = Field(description="员工 ID")
+    name: str = Field(description="员工姓名")
+    sal: float = Field(description="员工基本工资")
+    bonus: int = Field(description="员工津贴和奖金")
+    is_leave: bool = Field(description="员工是否离职")
+    gender: str = Field(description="员工性别", pattern="^男$|^女$")
+    entry_date: date = Field(description="入职日期")
+
